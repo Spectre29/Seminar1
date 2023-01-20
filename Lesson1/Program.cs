3ffadba863f27299823233ecf9895fbb5723b0ee
Spectre29 <ruslan.ibragimov.2003@gmail.com>
2023-01-20 16:26:22 +0500

    add 0 task

--- Lesson1/Program.cs
+++ Lesson1/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = int.Parse(Console.ReadLine());
+
+int result = number*number; 
+
+Console.WriteLine(result);
